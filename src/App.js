fc417481164aa1cfbe294a1b446518776ea1afc5
leceido <leceido@gmail.com>
2023-01-30 16:18:25 -0300

    Navbar e Footer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from "./components/pages/Home";
+import NewProject from "./components/pages/NewProject";
+import Company from "./components/pages/Company"
+import Contact from "./components/pages/Contact"
+import Container from "./components/layout/Container";
+import Navbar from "./components/layout/Navbar"
+import Footer from "./components/layout/Footer"
+import Projects from "./components/pages/Projects"
+
 function App() {
   return (
-    <div>
-      <p>Costs</p>
-    </div>
+    <Router>
+      <Navbar />
+      <Container customClass="min-height">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/company" element={<Company />} />
+          <Route exact path="/contact" element={<Contact />} />
+          <Route exact path="/newproject" element={<NewProject />} />
+          <Route exact path="/projects" element={<Projects />}/>
+        </Routes>
+      </Container>
+      <Footer />
+    </Router>
   );
 }
 
