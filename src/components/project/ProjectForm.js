0c23f7b1120a1f94a14495acac43429062ab8c42
leceido <leceido@gmail.com>
2023-01-31 16:25:51 -0300

    Formulario de projetos e Home page

---
+++ src/components/project/ProjectForm.js
@@ -0,0 +1,27 @@
+import Input from '../form/Input'
+import Select from '../form/Select'
+import SubmitButton from '../form/SubmitButton'
+import styles from './ProjectForm.module.css'
+
+function ProjectForm({btnText}) {
+    return (
+        <form className={styles.form}>
+            <Input 
+                type="text" 
+                text="Nome do Projeto" 
+                name="name" 
+                placeholder="Insira o nome do projeto"
+            />
+            <Input 
+                type="number" 
+                text="Orcamento do Projeto" 
+                name="budget" 
+                placeholder="Insira o orcamento total"
+            />
+            <Select name="category_id" text="Selecione a categoria"/>
+            <SubmitButton text={btnText}/>
+        </form>
+    )
+}
+
+export default ProjectForm
