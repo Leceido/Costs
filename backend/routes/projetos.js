64e08aee7e3ecc1a2bdaaaf14430f3df35c4d91d
leceido <leceido@gmail.com>
2023-02-04 17:04:38 -0300

    Inserindo Projetos no DB

--- backend/routes/projetos.js
+++ backend/routes/projetos.js
@@ -6,5 +6,38 @@ const Categoria = mongoose.model('categorias')
 require('../models/Projeto')
 const Projeto = mongoose.model('projetos')
 
+router.get('/', async (req,res, next) => {
+    try {
+        const projetos = await Projeto.find()
+        res.status(200).send({projetos})
+    } catch (err) {
+        res.status(500).send({error: err})
+    }
+})
+
+router.post('/', (req, res, next) => {
+    const novoProjeto = {
+        nome: req.body.name,
+        categoria: req.body.category,
+        orcamento: req.body.budget
+    }
+
+    new Projeto(novoProjeto).save().then(() => {
+        const response = {
+            mensagem: "Projeto criado",
+            projeto: novoProjeto,
+            request: {
+                tipo: 'GET',
+                descricao: 'Retorna todos os Projetos',
+                url: 'http://localhost:5000/projetos/'
+            }
+        }
+        res.status(201).send({response})
+    }).catch((err) => {
+        res.status(500).send({
+            error: err
+        })
+    })
+})
 
 module.exports = router
--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -4,7 +4,7 @@ function Select({text, name, options, handleOnChange, value}) {
     return (
         <div className={styles.form_control}>
             <label htmlFor={name}>{text}: </label>
-            <select name={name} id={name}>
+            <select name={name} id={name} onChange={handleOnChange} value={value || ''}>
                 <option>Selecione uma opcao</option>
                 {options.map(categoria => {
                     return <option value={categoria._id} key={categoria._id}>{categoria.nome}</option>
