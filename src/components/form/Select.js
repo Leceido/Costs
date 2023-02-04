7c528fb99e6af3939896511d8e51b93589aecdbc
leceido <leceido@gmail.com>
2023-02-04 15:32:47 -0300

    Select de Categorias dinamico

--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -1,30 +1,14 @@
-import { useState } from 'react';
 import styles from './Select.module.css'
 
 function Select({text, name, options, handleOnChange, value}) {
-
-    const [categories, setCategories] = useState()
-
-    function chooseOption(e) {
-        fetch("http://localhost:5000/categorias", {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        })
-        .then((resp) => resp.json())
-        .then((data) => {
-            setCategories(data)
-            console.log(categories.categorias);
-        })
-        .catch((err) => console.log(err))
-    }
-
     return (
         <div className={styles.form_control}>
             <label htmlFor={name}>{text}: </label>
-            <select name={name} id={name} onClick={chooseOption}>
-                
+            <select name={name} id={name}>
+                <option>Selecione uma opcao</option>
+                {options.map(categoria => {
+                    return <option value={categoria._id} key={categoria._id}>{categoria.nome}</option>
+                })}
             </select>
         </div>
     )
--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,9 +1,25 @@
+import { useEffect, useState } from 'react'
 import Input from '../form/Input'
 import Select from '../form/Select'
 import SubmitButton from '../form/SubmitButton'
 import styles from './ProjectForm.module.css'
 
 function ProjectForm({btnText}) {
+
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:5000/categorias', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        }).then((result) => {
+            result.json().then((res) => {
+                setCategories(res.categorias)
+            })
+        }).catch((err) => console.log(err))
+    }, [])
 
     return (
         <form className={styles.form}>
@@ -19,7 +35,7 @@ function ProjectForm({btnText}) {
                 name="budget" 
                 placeholder="Insira o orcamento total"
             />
-            <Select name="category_id" text="Selecione a categoria"/>
+            <Select name="category_id" text="Selecione a categoria" options={categories}/>
             <SubmitButton text={btnText}/>
         </form>
     )
